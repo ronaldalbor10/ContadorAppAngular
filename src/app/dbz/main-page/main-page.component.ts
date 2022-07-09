f952caf6493c5471b016afc6a4f7b39a92b92e44
Ronald Javier Albor Cardenas <ralbor@nitrofert.com.co>
2022-07-09 11:08:57 -0500

    Manejo de formulario con FormsModule

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -3,13 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main-page-dbz',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  
 })
-export class MainPageComponent implements OnInit {
+export class MainPageComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  agregar(){
+    
+    console.log("Hey");
   }
 
 }
